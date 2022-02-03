d3b161c93c39b9500fb07f61cc5fbe7f8a3a1eb6
Gabriel Cavalcante <gabrielcalk@outlook.com>
2022-02-03 11:23:20 -0500

    refactored adding helper methods

--- alien_invasion_game.py
+++ alien_invasion_game.py
@@ -32,23 +32,33 @@ class AlienInvasion:
     def run_game(self):
         # Start the main loop for the game
         while True:
-            # An event is an action that the user performs while playing the game, such as pressing a key or moving the mouse.
-            # To access the events that Pygame detects, we’ll use the pygame.event.get() function
-            # This function returns a list of events that have taken place since the last time this function was called.
-            # Any keyboard or mouse event will cause this for loop to run.
-            for event in pygame.event.get():
-                # when the player clicks the game window’s close button, a pygame.QUIT event is detected and we call sys.exit() to exit the game
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            # Checking events
+            self._check_events()
+            # updating the screen
+            self._update_screen()
 
-            # We fill the screen with the background color using the fill() method, which acts on a surface and takes only one argument: a color.
-            self.screen.fill(self.settings.bg_color)
-            
-            self.ship.blitme()
 
-            # The call to pygame.display.flip() tells Pygame to make the most recently drawn screen visible
-            # When we move the game elements around, pygame.display.flip() continually updates the display to show the new positions of game elements and hides the old ones, creating the illusion of smooth movement.
-            pygame.display.flip()
+    # A helper methods does work inside a class but isn’t meant to be called through an instance.
+    # In Python, a single leading underscore indicates a helper method
+    def _check_events(self):
+        # An event is an action that the user performs while playing the game, such as pressing a key or moving the mouse.
+        # To access the events that Pygame detects, we’ll use the pygame.event.get() function
+        # This function returns a list of events that have taken place since the last time this function was called.
+        # Any keyboard or mouse event will cause this for loop to run.
+        for event in pygame.event.get():
+            # when the player clicks the game window’s close button, a pygame.QUIT event is detected and we call sys.exit() to exit the game
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        # We fill the screen with the background color using the fill() method, which acts on a surface and takes only one argument: a color.
+        self.screen.fill(self.settings.bg_color)
+
+        self.ship.blitme()
+
+        # The call to pygame.display.flip() tells Pygame to make the most recently drawn screen visible
+        # When we move the game elements around, pygame.display.flip() continually updates the display to show the new positions of game elements and hides the old ones, creating the illusion of smooth movement.
+        pygame.display.flip()
 
 
 # We place run_game() in an if block that only runs if the file is called directly
